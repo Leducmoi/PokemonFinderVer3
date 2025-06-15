b1a54789796f0cc1f77e9ab3d77d458383b16f08
Le Duc Moi <leducmoi@dtu.edu.vn>
2025-06-15 20:07:52 +0700

    Done Header

--- PokemonFinderVer3/src/main.jsx
+++ PokemonFinderVer3/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Main from './Components/Main/Main.jsx'
+import './assets/scss/main.scss'
 
 
 createRoot(document.getElementById('root')).render(
