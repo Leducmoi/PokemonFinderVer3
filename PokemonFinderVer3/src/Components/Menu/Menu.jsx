3c59a56b14a911e95c7b348fbc0e0a8df21bde26
Le Duc Moi <leducmoi@dtu.edu.vn>
2025-06-18 00:04:26 +0700

    Set up menu

--- PokemonFinderVer3/src/Components/Menu/Menu.jsx
+++ PokemonFinderVer3/src/Components/Menu/Menu.jsx
@@ -1,9 +1,55 @@
-import { memo } from "react";
+import { memo,useState } from "react";
+import "./Menu.scss";
+import Slider from "@mui/material/Slider";
+
 
 function Menu() {
+  const [value1, setValue1] = useState([1, 1025]);
+
+  const handleChange1 = (event, newValue) => {
+    setValue1(newValue);
+  };
+
+  const valuetext = (value) => `${value}`;
   return (
     <>
-      <h1>Welcome to the Pokemon Finder Menu</h1>
+      <div className="menu">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 menu-filter" style={{ background: "lightblue", height: "100px" }}>
+              <div className="row" style={{ display: "flex", alignItems: "center", justifyContent: "space-around" }}>
+                <div className="col-md-4 menu-filter-option">TYPES</div>
+                <div className="col-md-4 menu-filter-option">ABILITIES</div>
+                <div className="col-md-4 menu-filter-option">STATS</div>
+              </div>
+              <div className="row" style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+                <div className="col-md-3 menu-filter-reset">RESET</div>
+                <div className="col-md-7 menu-filter-IdFilterSlider">
+                  <Slider
+                    getAriaLabel={() => 'Minimum distance'}
+                    value={value1}
+                    onChange={handleChange1}
+                    valueLabelDisplay="auto"
+                    getAriaValueText={valuetext}
+                    disableSwap
+                    min={1}
+                    max={1025}
+                  />
+
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3 menu-other" style={{ background: "lightgreen", height: "100px" }}></div>
+            <div className="col-md-3 menu-search" style={{ background: "lightcoral", height: "100px" }}>
+              <input
+                type="text"
+                className="form-control menu-search-input"
+                placeholder="Search by name, type, ability, etc."
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
