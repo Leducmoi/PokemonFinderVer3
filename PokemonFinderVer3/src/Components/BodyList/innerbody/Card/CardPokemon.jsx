60540edf1abfc3a40186c2173260120115be395e
Le Duc Moi <leducmoi@dtu.edu.vn>
2025-06-19 21:30:09 +0700

    Basic Fetch và Render card

---
+++ PokemonFinderVer3/src/Components/BodyList/innerbody/Card/CardPokemon.jsx
@@ -0,0 +1,25 @@
+import { memo } from "react";
+import "./CardPokemon.scss";
+
+function CardPokemon({ id, name, image, types }) {
+    return (
+        <div className="card-pokemon">
+            <div className="card-pokemon__img">
+                <img src={image} alt={name} />
+            </div>
+            <div className="card-pokemon__info">
+                <p>#{String(id).padStart(4, '0')}</p>
+                <h3>{name}</h3>
+                <div>
+                    {types.map((type) => (
+                        <span key={type} className={`type-badge type-${type}`}>
+                            {type} &nbsp;
+                        </span>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default memo(CardPokemon);
